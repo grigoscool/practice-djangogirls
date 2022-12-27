53ed882bb95dbf1a2e2f216c0d86f960f828f915
Sergey Grigorev <grigoscool@mail.ru>
2022-12-27 16:09:34 +0300

    secon com

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("-created_date")
+    pub_post = []
+    for post in posts:
+        if post.published_date:
+            pub_post.append(post)
+    context = {'pub_post': pub_post}
+    return render(request, 'blog/index.html', context)
