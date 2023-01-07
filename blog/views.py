02e1d3ab20849f960f811ffa06d1352db33c096a
Sergey Grigorev <grigoscool@mail.ru>
2023-01-07 17:36:44 +0300

    TIK-002

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
         return redirect('post_detail', pk=post.pk)
     else:
@@ -41,9 +40,12 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
         return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_new.html', {'form': form})
+
+def show_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/draft.html', {'posts': posts})
